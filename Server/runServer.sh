ea9b399d9309ea76817ccaca2faf5779f906c2db
dginovker <dcress01@uoguelph.ca>
2019-11-05 16:50:12 +0000

    make runServer import the correct plugin folder

--- Server/runServer.sh
+++ Server/runServer.sh
@@ -1,5 +1,5 @@
 rm -rf org
 cp -r ../CompiledServer/production/09Scape/org .
 rm -rf plugin/
-cp -r ../CompiledServer/production/09Scape/org/crandor/plugin .
+cp -r ../CompiledServer/production/09Scape/plugin/ .
 java -server -cp ".:./bin;.:./data/libs/*" org.crandor.Server 
