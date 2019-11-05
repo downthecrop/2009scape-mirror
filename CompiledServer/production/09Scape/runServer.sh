dda18380602262e783602cd170dc9a980a57b2ba
dginovker <dcress01@uoguelph.ca>
2019-11-05 11:47:06 -0500

    Update .gitignore to submit new compiles + missing class compiles

--- CompiledServer/production/09Scape/runServer.sh
+++ CompiledServer/production/09Scape/runServer.sh
@@ -1,3 +1,5 @@
 rm -rf org
 cp -r ../CompiledServer/production/09Scape/org .
+rm -rf plugin/
+cp -r ../CompiledServer/production/09Scape/org/crandor/plugin .
 java -server -cp ".:./bin;.:./data/libs/*" org.crandor.Server 
