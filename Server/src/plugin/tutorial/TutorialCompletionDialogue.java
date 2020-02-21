c3a8f5502e60fd1e13bc2e9008e1c0e006acf117
Qweqker <qweqker@mail.com>
2020-02-21 14:13:36 -0500

    Added a ton of dialogue for Iron Man mode Added the ability to change ironman mode Added the ability to remove ironman mode Added some checks to see if the player was too high level or had too many quest points to change Iron man mode Added some checks to see if the player has died as an iron man Added check to see if player has an empty bank when changing to uim Changed some case numbers around Added a comment to denote a possibly unnecessary line of code

--- Server/src/plugin/tutorial/TutorialCompletionDialogue.java
+++ Server/src/plugin/tutorial/TutorialCompletionDialogue.java
@@ -201,7 +201,7 @@ public class TutorialCompletionDialogue extends DialoguePlugin {
 						case 3:
 							npc("You have chosen the " + (buttonId == 1 ? "Standard" : (buttonId == 2 ? "<col=8A0808>Hardcore</col>" : "<col=ECEBEB>Ultimate</col>")) + " Ironman mode.");
 							player.setAttribute("ironMode", IronmanMode.values()[buttonId]);
-							player.getSavedData().getActivityData().setHardcoreDeath(0);
+							player.getSavedData().getActivityData().setHardcoreDeath(0); //might not be necessary?
 							stage = 516;
 							break;
 						case 4:
