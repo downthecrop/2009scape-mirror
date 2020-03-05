050225cc243202065a9dc4b5b95d48f9e45edf02
qahowers <dragonslayer9000@gmail.com>
2020-03-04 19:58:12 -0500

    Adds attack bonus to cup of tea Adds ability to remove-legs on swamp toads

--- Server/src/plugin/consumable/CupofTeaPlugin.java
+++ Server/src/plugin/consumable/CupofTeaPlugin.java
@@ -3,6 +3,8 @@ package plugin.consumable;
 import org.crandor.game.content.global.consumable.ConsumableProperties;
 import org.crandor.game.content.global.consumable.Consumables;
 import org.crandor.game.content.global.consumable.Drink;
+import org.crandor.game.content.skill.SkillBonus;
+import org.crandor.game.content.skill.Skills;
 import org.crandor.game.node.entity.player.Player;
 import org.crandor.plugin.InitializablePlugin;
 import org.crandor.game.node.item.Item;
@@ -14,6 +16,11 @@ import org.crandor.game.node.item.Item;
  */
 @InitializablePlugin
 public final class CupofTeaPlugin extends Drink {
+
+	/**
+	 * Represents the skill bonuses for each beer.
+	 */
+	private SkillBonus[] bonuses;
 
 	/**
 	 * Represents the force chat to use.
@@ -30,12 +37,19 @@ public final class CupofTeaPlugin extends Drink {
 	 * Constructs a new {@code CupofTeaPlugin} {@code Object}.
 	 */
 	public CupofTeaPlugin() {
-		super(712, new ConsumableProperties(2, 1980));
+		super(712, new ConsumableProperties(3, 1980));
+		this.bonuses = new SkillBonus[1];
+		this.bonuses[0] = new SkillBonus(Skills.ATTACK, 0, 3);
 	}
 
 	@Override
 	public void consume(final Item item, final Player player) {
 		player.sendChat(CHAT);
+		if (this.bonuses != null) {
+			for (SkillBonus b : bonuses) {
+				addBonus(player, b);
+			}
+		}
 		super.remove(player, item);
 	}
 
