bbcf3e86d2cc1d61a1fbc4b7cf56a325f5a5a296
Ceikry <ironceikry@gmail.com>
2020-03-12 23:46:07 -0500

    Added basic dialogue for black jack seller

--- Server/src/plugin/interaction/city/pollnivneach/BlackJackSeller.java
+++ Server/src/plugin/interaction/city/pollnivneach/BlackJackSeller.java
@@ -1,4 +1,42 @@
 package plugin.interaction.city.pollnivneach;
 
-public class BlackJackSeller {
+import org.crandor.game.content.dialogue.DialoguePlugin;
+import org.crandor.game.node.entity.player.Player;
+import org.crandor.plugin.InitializablePlugin;
+
+/**
+ * Stand-in dialogue for Black Jack Seller til blackjacking/the quest is implemented
+ * @author ceik
+ */
+@InitializablePlugin
+public class BlackJackSeller extends DialoguePlugin {
+    public BlackJackSeller(){
+        /**
+         * empty.
+         */
+    }
+    public BlackJackSeller(Player player){super(player);}
+
+    @Override
+    public DialoguePlugin newInstance(Player player){return new BlackJackSeller(player);}
+
+    @Override
+    public boolean open(Object... args){
+        npc("I'm not interested in selling to you. Not yet...");
+        stage = 0;
+        return true;
+    }
+
+    @Override
+    public boolean handle(int intefaceId, int objectId){
+        switch(stage){
+            case 0:
+                end();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public int[] getIds() {return new int[] {2548};}
 }
