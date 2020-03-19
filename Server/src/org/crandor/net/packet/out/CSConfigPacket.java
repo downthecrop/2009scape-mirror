e6e190ff23b144e3900d93d4c46c13df65aee940
ceik <ironceikry@gmail.com>
2020-03-19 17:23:48 -0500

    Added world map!

--- Server/src/org/crandor/net/packet/out/CSConfigPacket.java
+++ Server/src/org/crandor/net/packet/out/CSConfigPacket.java
@@ -7,11 +7,20 @@ import org.crandor.net.packet.context.CSConfigContext;
 /**
  * The outgoing packet for client script configs.
  * 
- * @author Torchic
+ * @author Snickerize
  */
 public class CSConfigPacket implements OutgoingPacket<CSConfigContext> {
 
 	@Override
+	public void send(CSConfigContext context) {
+		IoBuffer buffer = new IoBuffer(65);
+		buffer.putLEShort(context.getPlayer().getInterfaceManager().getPacketCount(1));
+		buffer.putC((byte) context.getValue());
+		buffer.putLEShortA(context.getId());
+		context.getPlayer().getDetails().getSession().write(buffer);
+	}
+
+	/*@Override
 	public void send(CSConfigContext context) {
 		IoBuffer buffer = new IoBuffer(115);
 		buffer.putShort(context.getId());
@@ -27,5 +36,5 @@ public class CSConfigPacket implements OutgoingPacket<CSConfigContext> {
 
 		buffer.putInt(context.getValue());
 		context.getPlayer().getDetails().getSession().write(buffer);
-	}
+	}*/
 }
--- Server/src/org/crandor/net/packet/out/RunScriptPacket.java
+++ Server/src/org/crandor/net/packet/out/RunScriptPacket.java
@@ -7,11 +7,11 @@ import org.crandor.net.packet.context.RunScriptContext;
 
 /**
  * The run script outgoing packet.
- * @author Emperor
+ * @author Snickerize
  */
 public class RunScriptPacket implements OutgoingPacket<RunScriptContext> {
 
-	@Override
+	/*@Override
 	public void send(RunScriptContext context) {
 		String string = context.getString();
 		Object[] objects = context.getObjects();
@@ -27,6 +27,27 @@ public class RunScriptPacket implements OutgoingPacket<RunScriptContext> {
 			}
 			j++;
 		}
+		buffer.putInt(context.getId());
+		context.getPlayer().getDetails().getSession().write(buffer);
+	}*/
+	@Override
+	public void send(RunScriptContext context) {
+		String types = context.getString();
+		Object[] objects = context.getObjects();
+		IoBuffer buffer = new IoBuffer(115, PacketHeader.SHORT);
+		buffer.putShort(context.getPlayer().getInterfaceManager().getPacketCount(1));
+
+		buffer.putString(types);
+		int j = 0;
+		for (int i = (types.length() - 1); i >= 0; i--) {
+			if (types.charAt(i) == 's') {
+				buffer.putString((String) objects[j]);
+			} else {
+				buffer.putInt((Integer) objects[j]);
+			}
+			j++;
+		}
+
 		buffer.putInt(context.getId());
 		context.getPlayer().getDetails().getSession().write(buffer);
 	}
