ad9b6ec1b954c8dbf4df9ecd8babe7b1a7d50b1d
CSS-Lletya <admin@lletya.com>
2020-03-29 00:19:34 -0400

    Update GoblinVillagePopulationPlugin.java

--- Server/src/plugin/interaction/object/GoblinVillagePopulationPlugin.java
+++ Server/src/plugin/interaction/object/GoblinVillagePopulationPlugin.java
@@ -1,9 +1,13 @@
 package plugin.interaction.object;
 
+import java.util.List;
+
 import org.crandor.cache.def.impl.ObjectDefinition;
 import org.crandor.game.interaction.OptionHandler;
 import org.crandor.game.node.Node;
+import org.crandor.game.node.entity.npc.NPC;
 import org.crandor.game.node.entity.player.Player;
+import org.crandor.game.world.map.RegionManager;
 import org.crandor.plugin.InitializablePlugin;
 import org.crandor.plugin.Plugin;
 
@@ -18,8 +22,14 @@ public class GoblinVillagePopulationPlugin extends OptionHandler {
 
 	@Override
 	public boolean handle(Player player, Node node, String option) {
-		int population = 26; //TODO: Find out how to get all goblins in the area.
-		player.getDialogueInterpreter().sendPlainMessage(false, "Welcome to Goblin Village.", "Current population: " + population);
+		int population = 2;
+		final List<NPC> npcs = RegionManager.getLocalNpcs(player);
+		for (NPC n : npcs) {
+			if (n.getName().equals("Goblin")) {
+				population++;
+				player.getDialogueInterpreter().sendPlainMessage(false, "Welcome to Goblin Village.", "Current population: " + population);
+			}
+		}
 		return true;
 	}
 
