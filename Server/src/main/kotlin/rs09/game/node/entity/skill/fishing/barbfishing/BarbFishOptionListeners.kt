42a4e99f785dc5d4782531d73d87c390331b7bee
Ceikry <ironceikry@gmail.com>
2021-03-12 00:31:00 -0600

    re-added barbarian fishing

---
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/fishing/barbfishing/BarbFishOptionListeners.kt
@@ -0,0 +1,30 @@
+package rs09.game.node.entity.skill.fishing.barbfishing
+
+import core.game.node.item.Item
+import rs09.game.interaction.OptionListener
+
+class BarbFishOptionListeners : OptionListener() {
+    override fun defineListeners() {
+
+        on(25268,OBJECT,"search"){player,_ ->
+            if(player.getAttribute("barbtraining:fishing",false) == true){
+                if(!player.inventory.containsItem(Item(11323))){
+                    player.inventory.add(Item(11323))
+                    player.sendMessage("Under the bed you find a fishing rod.")
+                } else {
+                    player.sendMessage("You find nothing under the bed")
+                }
+            } else {
+                player.sendMessage("Maybe I should speak to Otto before looking under his bed.")
+            }
+            return@on true
+        }
+
+        on(1176,NPC,"fish"){player,_ ->
+            player.pulseManager.run(BarbFishingPulse(player))
+            player.sendMessage("You attempt to catch a fish...")
+            return@on true
+        }
+
+    }
+}
