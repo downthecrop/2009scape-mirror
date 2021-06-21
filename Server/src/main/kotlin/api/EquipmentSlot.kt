8ea95973abe39df7fa50b4194d1f41aa7850cefe
Ceikry <ironceikry@gmail.com>
2021-06-21 18:26:29 -0500

    Fix ring of forging bug, ported ContentAPI.kt

---
+++ Server/src/main/kotlin/api/EquipmentSlot.kt
@@ -0,0 +1,18 @@
+package api
+
+enum class EquipmentSlot {
+    HAT,
+    CAPE,
+    AMULET,
+    WEAPON,
+    CHEST,
+    SHIELD,
+    HIDDEN_1,
+    LEGS,
+    HIDDEN_2,
+    HANDS,
+    FEET,
+    HIDDEN_3,
+    RING,
+    AMMO
+}
