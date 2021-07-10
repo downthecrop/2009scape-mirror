c663735c912e5351f6df62ec631e4204fc47c999
ceikry <ironceikry@gmail.com>
2021-07-10 14:37:57 -0500

    Made the guard tower ladder in neitiznot climbable

--- Server/src/main/kotlin/rs09/game/interaction/region/rellekka/NeitiznotListeners.kt
+++ Server/src/main/kotlin/rs09/game/interaction/region/rellekka/NeitiznotListeners.kt
@@ -5,6 +5,7 @@ import core.game.node.Node
 import core.game.node.entity.npc.NPC
 import core.game.node.entity.player.Player
 import core.game.node.item.Item
+import core.game.world.map.Location
 import core.game.world.map.zone.MapZone
 import core.game.world.map.zone.ZoneBorders
 import org.rs09.consts.NPCs
@@ -23,5 +24,7 @@ class NeitiznotListeners : InteractionListener() {
         }
 
         ContentAPI.registerMapZone(zone, ZoneBorders(2313,3786,2331,3802))
+        ContentAPI.addClimbDest(Location.create(2363, 3799, 0), Location.create(2364, 3799, 2))
+        ContentAPI.addClimbDest(Location.create(2363, 3799, 2), Location.create(2362, 3799, 0))
     }
 }
