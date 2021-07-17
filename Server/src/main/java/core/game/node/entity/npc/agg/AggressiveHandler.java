3c9f5403c540d0977e5c2452a622da8d7b83fbec
ceikry <ironceikry@gmail.com>
2021-07-16 21:04:40 -0500

    Implemented NPC aggression tolerance

--- Server/src/main/java/core/game/node/entity/npc/agg/AggressiveHandler.java
+++ Server/src/main/java/core/game/node/entity/npc/agg/AggressiveHandler.java
@@ -81,9 +81,10 @@ public final class AggressiveHandler {
 		Entity target = behavior.getLogicalTarget(entity, behavior.getPossibleTargets(entity, radius));
 		if (target instanceof Player) {
 			if (target.getAttribute("ignore_aggression", false)) {
-				if (((Player) target).getRights().equals(Rights.ADMINISTRATOR)) {
-					return false;
-				}
+				return false;
+			}
+			if (((Player) target).getRights().equals(Rights.ADMINISTRATOR)) {
+				return false;
 			}
 		}
 		if (target != null) {
