654cafbba3bad171a84086c6f1e2d7a49ab668b2
vk <vk@kretov.org>
2021-10-02 18:53:54 -0700

    implement fire titan scroll

--- Server/src/main/java/core/game/node/entity/npc/familiar/FireTitanNPC.java
+++ Server/src/main/java/core/game/node/entity/npc/familiar/FireTitanNPC.java
@@ -1,5 +1,6 @@
 package core.game.node.entity.npc.familiar;
 
+import core.game.node.entity.skill.Skills;
 import core.game.node.entity.skill.summoning.familiar.Familiar;
 import core.game.node.entity.skill.summoning.familiar.FamiliarSpecial;
 import core.plugin.Initializable;
@@ -12,6 +13,8 @@ import core.game.node.entity.player.Player;
 @Initializable
 public class FireTitanNPC extends Familiar {
 
+	private static final int scrollHealAmount = 8;
+	private static final double  scrollDefenceBoostPercent = 12.5;
 	/**
 	 * Constructs a new {@code FireTitanNPC} {@code Object}.
 	 */
@@ -33,9 +36,24 @@ public class FireTitanNPC extends Familiar {
 		return new FireTitanNPC(owner, id);
 	}
 
+	/**
+		raises defence by 12.5% and heals 8 hp (with ability to over heal)
+	 */
 	@Override
 	protected boolean specialMove(FamiliarSpecial special) {
-		return false;
+		int currentDefenceLevel = owner.getSkills().getLevel(Skills.DEFENCE);
+		owner.getSkills().updateLevel(Skills.DEFENCE, (int)(scrollDefenceBoostPercent * currentDefenceLevel), 112);
+		int currentHp = owner.getSkills().getLifepoints();
+		int maxHp = owner.getSkills().getMaximumLifepoints() + scrollHealAmount;
+		int healAmount = Math.min(maxHp - currentHp, scrollHealAmount);
+		if (healAmount> 0) {
+			owner.getSkills().healNoRestrictions(healAmount);
+			return true;
+		}
+		else {
+			owner.sendMessage("You are already at maximum hitpoints!");
+			return false;
+		}
 	}
 
 	@Override
