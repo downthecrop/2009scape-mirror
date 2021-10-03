7a1416583ec8a013d9b09dd24e5dbd7c6104bdca
vk <vk@kretov.org>
2021-10-02 19:23:16 -0700

    other titans

--- Server/src/main/java/core/game/node/entity/npc/familiar/FireTitanNPC.java
+++ Server/src/main/java/core/game/node/entity/npc/familiar/FireTitanNPC.java
@@ -1,5 +1,6 @@
 package core.game.node.entity.npc.familiar;
 
+import core.game.node.entity.combat.equipment.WeaponInterface;
 import core.game.node.entity.skill.Skills;
 import core.game.node.entity.skill.summoning.familiar.Familiar;
 import core.game.node.entity.skill.summoning.familiar.FamiliarSpecial;
@@ -11,10 +12,8 @@ import core.game.node.entity.player.Player;
  * @author Aero
  */
 @Initializable
-public class FireTitanNPC extends Familiar {
+public class FireTitanNPC extends ElementalTitanNPC {
 
-	private static final int scrollHealAmount = 8;
-	private static final double  scrollDefenceBoostPercent = 12.5;
 	/**
 	 * Constructs a new {@code FireTitanNPC} {@code Object}.
 	 */
@@ -28,32 +27,12 @@ public class FireTitanNPC extends Familiar {
 	 * @param id The id.
 	 */
 	public FireTitanNPC(Player owner, int id) {
-		super(owner, id, 6200, 12802, 20);
+		super(owner, id, 6200, 12802, 20, WeaponInterface.STYLE_CAST);
 	}
 
 	@Override
 	public Familiar construct(Player owner, int id) {
 		return new FireTitanNPC(owner, id);
-	}
-
-	/**
-		raises defence by 12.5% and heals 8 hp (with ability to over heal)
-	 */
-	@Override
-	protected boolean specialMove(FamiliarSpecial special) {
-		int currentDefenceLevel = owner.getSkills().getLevel(Skills.DEFENCE);
-		owner.getSkills().updateLevel(Skills.DEFENCE, (int)(scrollDefenceBoostPercent * currentDefenceLevel), 112);
-		int currentHp = owner.getSkills().getLifepoints();
-		int maxHp = owner.getSkills().getMaximumLifepoints() + scrollHealAmount;
-		int healAmount = Math.min(maxHp - currentHp, scrollHealAmount);
-		if (healAmount> 0) {
-			owner.getSkills().healNoRestrictions(healAmount);
-			return true;
-		}
-		else {
-			owner.sendMessage("You are already at maximum hitpoints!");
-			return false;
-		}
 	}
 
 	@Override
