e6e8ae026728176c7d8aeb2bf6cf0c15c0bd1c2d
vk <vk@kretov.org>
2021-10-02 22:01:00 -0700

    add lava titan teleport to lava maze (near kbd lair)

---
+++ Server/src/main/java/core/game/content/dialogue/LavaTitanDialogue.java
@@ -0,0 +1,81 @@
+package core.game.content.dialogue;
+
+import core.game.node.entity.npc.NPC;
+import core.game.node.entity.player.Player;
+import core.game.node.entity.player.link.TeleportManager.TeleportType;
+import core.game.node.entity.skill.summoning.familiar.Familiar;
+import core.game.world.map.Location;
+import core.game.world.map.zone.impl.WildernessZone;
+import core.plugin.Initializable;
+
+/**
+ * Represents the spirit graahk's dialogue
+ * @author Splinter
+ * @version 1.0
+ */
+@Initializable
+public final class LavaTitanDialogue extends DialoguePlugin {
+
+	/**
+	 * Constructs a new {@code GertrudesCatDialogue} {@code Object}.
+	 */
+	public LavaTitanDialogue() {
+		/**
+		 * empty.
+		 */
+	}
+
+	/**
+	 * Constructs a new {@code GertrudesCatDialogue} {@code Object}.
+	 * @param player the player.
+	 */
+	public LavaTitanDialogue(Player player) {
+		super(player);
+	}
+
+	@Override
+	public DialoguePlugin newInstance(Player player) {
+		return new LavaTitanDialogue(player);
+	}
+
+	@Override
+	public boolean open(Object... args) {
+		npc = (NPC) args[0];
+		if (!(npc instanceof Familiar)) {
+			return false;
+		}
+		final Familiar fam = (Familiar) npc;
+		if (fam.getOwner() != player) {
+			player.getPacketDispatch().sendMessage("This is not your familiar.");
+			return true;
+		} else {
+			interpreter.sendOptions("Select an Option", "Chat", "DANGER: Teleport (lava maze, level 38 wilderness)");
+		}
+		return true;
+	}
+
+	@Override
+	public boolean handle(int interfaceId, int buttonId) {
+		switch (buttonId) {
+		case 1:
+			player.sendMessage("The lava titan does not feel like talking now.");
+			end();
+			break;
+		case 2:
+			if (!WildernessZone.checkTeleport(player, 20)) {
+				player.sendMessage("You cannot teleport with the Graahk above level 20 wilderness.");
+				end();
+			} else {
+				player.getTeleporter().send(new Location(3048, 3820), TeleportType.NORMAL);
+				end();
+			}
+			break;
+		}
+		return true;
+	}
+
+	@Override
+	public int[] getIds() {
+		return new int[] { 8700 };
+	}
+}
