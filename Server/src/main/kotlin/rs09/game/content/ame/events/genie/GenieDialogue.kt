936336d5365547ff69da83b8039fb1c061cbc0da
skelsoft <9147770-skelsoft@users.noreply.gitlab.com>
2021-10-18 09:38:38 +1100

    Fix: Genie Random
    Applied code changes as requested by Ceikry

--- Server/src/main/kotlin/rs09/game/content/ame/events/genie/GenieDialogue.kt
+++ Server/src/main/kotlin/rs09/game/content/ame/events/genie/GenieDialogue.kt
@@ -1,5 +1,7 @@
 package rs09.game.content.ame.events.genie
 
+import api.ContentAPI
+import core.game.content.dialogue.FacialExpression
 import core.cache.def.impl.ItemDefinition
 import core.game.component.Component
 import core.game.node.entity.combat.ImpactHandler
@@ -11,10 +13,8 @@ import rs09.tools.END_DIALOGUE
 class GenieDialogue : DialogueFile() {
     override fun handle(componentID: Int, buttonID: Int) {
         val assigned = player!!.getAttribute("genie:item",0)
-        npc("Ah, so you are there, @name. I'm so glad you summoned me.", "Please take this lamp and make your wish.")
-        if(!player!!.inventory.add(Item(assigned))){
-            GroundItemManager.create(Item(assigned),player)
-        }
+        npcl(FacialExpression.NEUTRAL, "Ah, so you are there, @name. I'm so glad you summoned me. Please take this lamp and make your wish.")
+		ContentAPI.addItemOrDrop(player!!, assigned)
         player!!.antiMacroHandler.event?.terminate()
         stage = END_DIALOGUE
     }
