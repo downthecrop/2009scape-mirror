4c870a53dad111bc4627381dea645385911e3b99
ceikry <ironceikry@gmail.com>
2021-11-01 07:16:45 -0500

    Fixed

--- Server/src/main/java/core/game/content/global/action/SpecialLadders.java
+++ Server/src/main/java/core/game/content/global/action/SpecialLadders.java
@@ -46,7 +46,7 @@ public enum SpecialLadders implements LadderAchievementCheck {
     },
     SEERS_VILLAGE_SPINNING_HOUSE_ROOFTOP_DOWN(new Location(2715,3472,3), new Location(2714,3472,1)),
     ELEMENTAL_WORKSHOP_STAIRS_DOWN(Location.create(2710,3497, 0), Location.create(2713,9887, 0)),
-    ELEMENTAL_WORKSHOP_STAIRS_UP(Location.create(2714,9887, 0), Location.create(2709,3498, 0)),
+    ELEMENTAL_WORKSHOP_STAIRS_UP(Location.create(2714,9887, 0), Location.create(2709,3498, 0));
 
     private static HashMap<Location,Location> destinationMap = new HashMap<>();
     private static HashMap<Location,SpecialLadders> ladderMap = new HashMap<>();
