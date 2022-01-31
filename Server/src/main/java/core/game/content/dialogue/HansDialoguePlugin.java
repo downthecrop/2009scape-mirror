5d4d9c40356e784ccf24736b07f8ce9af045a176
skelsoft <9147770-skelsoft@users.noreply.gitlab.com>
2022-02-01 06:13:27 +1100

    fix hans
    hehe woops again this is why you review the mrs

--- Server/src/main/java/core/game/content/dialogue/HansDialoguePlugin.java
+++ Server/src/main/java/core/game/content/dialogue/HansDialoguePlugin.java
@@ -138,9 +138,9 @@ public final class HansDialoguePlugin extends DialoguePlugin {
 				switch(buttonId){
 					case 1:
 						if(player.getAttributes().containsKey("permadeath")){
-							options("1.0x", "2.5x", "Stay 5.0x", "(HCIM Only) 10x", "20x");
+							options("1.0x", "2.5x", "Stay 5.0x", "(HCIM Only) 10x");
 						} else {
-							options("1.0x", "2.5x", "Stay 5.0x", "10x", "20x");
+							options("1.0x", "2.5x", "Stay 5.0x");
 						}
 						stage++;
 						break;
@@ -180,14 +180,7 @@ public final class HansDialoguePlugin extends DialoguePlugin {
 						} else {
 							stage = 15;
 						}
-						break;
-					case 5:
-						if (player.newPlayer) {
-							player.getSkills().experienceMutiplier = 20.0;
-							stage = 14;
-						} else {
-							stage = 15;
-						}						
+						break;					
 				}
 				npc("One moment, please...");
 				break;
