149077bc8892bd8424486c16f21f38f42f0bc96b
enosich <evannosich@icloud.com>
2022-03-17 19:54:02 -0400

    Gnome Copter Advertisement is supposed to send a dialog box, not a message

--- Server/src/main/kotlin/rs09/game/interaction/region/lumbridge/GnomeCopterSignListener.kt
+++ Server/src/main/kotlin/rs09/game/interaction/region/lumbridge/GnomeCopterSignListener.kt
@@ -1,5 +1,6 @@
 package rs09.game.interaction.region.lumbridge
 
+import api.sendDialogue
 import api.sendMessage
 import org.rs09.consts.Scenery
 import rs09.game.interaction.InteractionListener
@@ -12,8 +13,7 @@ class GnomeCopterSignListener : InteractionListener() {
     val SIGN = Scenery.ADVERTISEMENT_30037
     override fun defineListeners() {
         on(SIGN, SCENERY, "read") { player, _ ->
-            sendMessage(player, "Come check our gnome copters up north! Disclaimer: EXTREMELY WIP")
-
+            sendDialogue(player, "Come check our gnome copters up north! Disclaimer: EXTREMELY WIP")
             return@on true
         }
     }
