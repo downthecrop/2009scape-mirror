b1d6ce49c8ef12ef3ba1d0644ce8452c1591dc9b
ceikry <ironceikry@gmail.com>
2022-03-27 15:47:07 -0500

    Implemented the specifications for the culinomancer chest defined in #252

--- Server/src/main/java/core/game/interaction/object/CulinoChestListener.kt
+++ Server/src/main/java/core/game/interaction/object/CulinoChestListener.kt
@@ -1,48 +1,36 @@
-package core.game.interaction.object;
+package core.game.interaction.`object`
 
-import core.cache.def.impl.SceneryDefinition;
-import core.game.interaction.OptionHandler;
-import core.game.node.Node;
-import core.game.node.entity.player.Player;
-import core.game.node.scenery.Scenery;
-import core.game.node.scenery.SceneryBuilder;
-import core.game.world.map.Location;
-import core.game.world.map.RegionManager;
-import core.plugin.Initializable;
-import core.plugin.Plugin;
-import rs09.game.system.config.ShopParser;
+import api.getUsedOption
+import core.game.world.map.RegionManager.getObject
+import core.game.content.global.shop.CulinomancerShop.openShop
+import core.plugin.Initializable
+import core.game.interaction.OptionHandler
+import core.plugin.Plugin
+import core.cache.def.impl.SceneryDefinition
+import core.game.content.global.shop.CulinomancerShop
+import core.game.node.scenery.SceneryBuilder
+import core.game.node.entity.player.Player
+import core.game.node.Node
+import core.game.world.map.Location
+import org.rs09.consts.Scenery
+import rs09.game.interaction.InteractionListener
 
 /**
- * Handles the culinomancer chest.
- * @author Vexia
+ * Handles the culino chest options.
+ * @author Ceikry
  */
-@Initializable
-public final class CulinomancerChestPliugin extends OptionHandler {
+class CulinoChestListener : InteractionListener() {
+    val CULINO_CHEST = Scenery.CHEST_12309
 
-	@Override
-	public Plugin<Object> newInstance(Object arg) throws Throwable {
-		SceneryDefinition.forId(12309).getHandlers().put("option:bank", this);
-		SceneryDefinition.forId(12309).getHandlers().put("option:buy-items", this);
-		SceneryDefinition.forId(12309).getHandlers().put("option:buy-food", this);
-		Scenery object = RegionManager.getObject(new Location(3219, 9623, 0));
-		SceneryBuilder.replace(object, object.transform(object.getId(), 3));
-		return this;
-	}
+    override fun defineListeners() {
+        on(CULINO_CHEST, SCENERY, "buy-items","buy-food"){player, _ ->
+            openShop(player, food = getUsedOption(player) == "buy-food")
+            return@on true
+        }
 
-	@Override
-	public boolean handle(Player player, Node node, String option) {
-		switch (option) {
-		case "bank":
-			player.getBank().open();
-			return true;
-		case "buy-items":
-			ShopParser.Companion.openUid(player, 178);
-			return true;
-		case "buy-food":
-			ShopParser.Companion.openUid(player, 177);
-			return true;
-		}
-		return true;
-	}
-
+        on(CULINO_CHEST, SCENERY, "bank"){player, _ ->
+            player.bank.open()
+            return@on true
+        }
+    }
 }
