305c2c84a32a7bc5e1fff4059cd034388ea646d0
ceikry <ironceikry@gmail.com>
2022-03-28 14:57:30 -0500

    Start of unification of plugin systems

--- Server/src/main/kotlin/rs09/plugin/ClassScanner.kt
+++ Server/src/main/kotlin/rs09/plugin/ClassScanner.kt
@@ -19,6 +19,7 @@ import rs09.game.node.entity.player.info.login.PlayerSaveParser
 import rs09.game.node.entity.player.info.login.PlayerSaver
 import rs09.game.node.entity.skill.magic.SpellListener
 import rs09.game.system.SystemLogger
+import rs09.game.system.command.Command
 import rs09.game.world.GameWorld
 import java.util.*
 import java.util.function.Consumer
