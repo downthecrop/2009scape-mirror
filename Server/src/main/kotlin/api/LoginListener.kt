8de7f8e03d198daceb104d6967c00861126cb673
ceikry <ironceikry@gmail.com>
2022-03-29 07:07:12 -0500

    Added some more javadoc

--- Server/src/main/kotlin/api/LoginListener.kt
+++ Server/src/main/kotlin/api/LoginListener.kt
@@ -6,6 +6,7 @@ interface LoginListener {
     /**
      * NOTE: This should NOT reference any non-static class-local variables.
      * If you need to access a player's specific instance, use an attribute.
+     * Alternatively, consider using an [api.events.EventHook] if applicable.
      */
     fun login(player: Player)
 }
