a2884a92f28c42a0d7e44a0d584abf20c3201c3a
enosich <evansaccounts@outlook.com>
2022-05-10 22:55:29 -0400

    Change hashmap to int array

--- Server/src/main/kotlin/rs09/game/node/entity/skill/cooking/SaucyKebabListener.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/cooking/SaucyKebabListener.kt
@@ -13,13 +13,14 @@ import rs09.game.interaction.InteractionListener
  */
 class SaucyKebabListener : InteractionListener {
     val sauce = Items.RED_HOT_SAUCE_4610
-    val kebabMap = hashMapOf(
-        Items.KEBAB_1971 to Items.KEBAB_1972,
-        Items.UGTHANKI_KEBAB_1883 to Items.UGTHANKI_KEBAB_1886
+    val kebabArr = intArrayOf(
+        Items.KEBAB_1971,
+        Items.UGTHANKI_KEBAB_1883,
+        Items.UGTHANKI_KEBAB_1885
     )
 
     override fun defineListeners() {
-        onUseWith(ITEM, kebabMap.keys.toIntArray(), sauce) { player, used, with ->
+        onUseWith(ITEM, kebabArr, sauce) { player, used, with ->
             if(removeItem(player, used.asItem()) && removeItem(player, with.asItem())) {
                 return@onUseWith addItem(player, Items.SUPER_KEBAB_4608)
             }
