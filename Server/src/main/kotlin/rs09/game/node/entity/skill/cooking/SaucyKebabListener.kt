8b2b9ac559da3f2b276aaaa3b70635a2328f4d47
enosich <evansaccounts@outlook.com>
2022-05-10 22:42:27 -0400

    Create kebab listener

---
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/cooking/SaucyKebabListener.kt
@@ -0,0 +1,29 @@
+package rs09.game.node.entity.skill.cooking
+
+
+import api.*
+
+import org.rs09.consts.Items
+import rs09.game.interaction.InteractionListener
+
+
+/**
+ * @author bushtail
+ * That's-a spicy kebab!
+ */
+class SaucyKebabListener : InteractionListener {
+    val sauce = Items.RED_HOT_SAUCE_4610
+    val kebabMap = hashMapOf(
+        Items.KEBAB_1971 to Items.KEBAB_1972,
+        Items.UGTHANKI_KEBAB_1883 to Items.UGTHANKI_KEBAB_1886
+    )
+
+    override fun defineListeners() {
+        onUseWith(ITEM, kebabMap.keys.toIntArray(), sauce) { player, used, with ->
+            if(removeItem(player!!, used.asItem()) && removeItem(player!!, with.asItem())) {
+                return@onUseWith addItem(player, Items.SUPER_KEBAB_4608)
+            }
+            return@onUseWith false
+        }
+    }
+}
