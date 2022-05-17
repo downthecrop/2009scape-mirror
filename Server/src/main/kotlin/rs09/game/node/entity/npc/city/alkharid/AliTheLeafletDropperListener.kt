d21704d1ca991a05334e02874d53a1cc091e0499
enosich <evansaccounts@outlook.com>
2022-05-17 18:18:20 -0400

    when(stage) goes inside when(it) so this fix should be the last for ali the leaflet dropper

--- Server/src/main/kotlin/rs09/game/node/entity/npc/city/alkharid/AliTheLeafletDropperListener.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/npc/city/alkharid/AliTheLeafletDropperListener.kt
@@ -2,7 +2,6 @@ package rs09.game.node.entity.npc.city.alkharid
 
 import api.addItem
 import api.openDialogue
-import api.sendNPCDialogue
 import core.game.content.dialogue.FacialExpression
 import core.game.node.entity.npc.NPC
 import org.rs09.consts.Items
@@ -14,14 +13,15 @@ import rs09.tools.END_DIALOGUE
 /**
  * @author bushtail
  */
+
 class AliTheLeafletDropperListener : InteractionListener {
     override fun defineListeners() {
-        on(NPCs.ALI_THE_LEAFLET_DROPPER_3680, "take-flyer") { player, node ->
+        on(NPCs.ALI_THE_LEAFLET_DROPPER_3680, NPC, "Take-flyer") { player, node ->
             if(player.inventory.containItems(Items.AL_KHARID_FLYER_7922)) {
-                openDialogue(player, DropperDialogue(201), node as NPC)
+                openDialogue(player, DropperDialogue(2), node as NPC)
             } else {
                 if(addItem(player, Items.AL_KHARID_FLYER_7922)) {
-                    openDialogue(player, DropperDialogue(101), node as NPC)
+                    openDialogue(player, DropperDialogue(1), node as NPC)
                 } else {
                     return@on false
                 }
@@ -33,11 +33,12 @@ class AliTheLeafletDropperListener : InteractionListener {
 
 class DropperDialogue(val it : Int) : DialogueFile() {
     override fun handle(componentID: Int, buttonID: Int) {
-        when(it){
-            101 -> npcl(FacialExpression.CHILD_NORMAL, "Here! Take one and let me get back to work.").also { stage++ }
-            102 -> npcl(FacialExpression.CHILD_THINKING, "I still have hundreds of these flyers to hand out. I wonder if Ali would notice if I quietly dumped them somewhere?").also { stage = END_DIALOGUE }
-
-            201 -> npcl(FacialExpression.CHILD_SUSPICIOUS, "Are you trying to be funny or has age turned your brain to mush? You already have a flyer!").also { stage = END_DIALOGUE }
+        when(it) {
+            1 -> when(stage) {
+                0 -> npcl(FacialExpression.CHILD_NORMAL, "Here! Take one and let me get back to work.").also { stage++ }
+                1 -> npcl(FacialExpression.CHILD_THINKING, "I still have hundreds of these flyers to hand out. I wonder if Ali would notice if I quietly dumped them somewhere?").also { stage = END_DIALOGUE }
+            }
+            2 -> npcl(FacialExpression.CHILD_SUSPICIOUS, "Are you trying to be funny or has age turned your brain to mush? You already have a flyer!").also { stage = END_DIALOGUE }
         }
     }
 
