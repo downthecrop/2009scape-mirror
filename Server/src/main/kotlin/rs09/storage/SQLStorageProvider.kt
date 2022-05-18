8f3192df5ca2de5affb0b7a53174253c0cd0f552
ceikry <ironceikry@gmail.com>
2022-05-17 20:28:45 -0500

    Only update changed fields

--- Server/src/main/kotlin/rs09/storage/SQLStorageProvider.kt
+++ Server/src/main/kotlin/rs09/storage/SQLStorageProvider.kt
@@ -1,6 +1,7 @@
 package rs09.storage
 
 import rs09.auth.UserAccountInfo
+import rs09.game.system.SystemLogger
 import java.lang.Long.max
 import java.sql.*
 
@@ -59,6 +60,7 @@ class SQLStorageProvider : AccountStorageProvider {
                 result.getLong(16)   .let { userData.lastLogin = max(0L, it) }
                 result.getBoolean(17).let { userData.online = it }
 
+                userData.setInitialReferenceValues()
                 return userData
             } else {
                 return UserAccountInfo.createDefault()
@@ -103,13 +105,17 @@ class SQLStorageProvider : AccountStorageProvider {
             if (result.next()) {
                 info.uid = result.getInt(1)
             }
+            info.setInitialReferenceValues()
         }
     }
 
     override fun update(info: UserAccountInfo) {
+        val (updatedFields, rawData) = info.getChangedFields()
+        val updateQueryString = buildUpdateInfoQuery(updatedFields)
         val conn = getConnection()
+
         conn.use {
-            val compiledUpdateInfoQuery = it.prepareStatement(updateInfoQuery)
+            val compiledUpdateInfoQuery = it.prepareStatement(updateQueryString)
             val emptyInfo = UserAccountInfo.createDefault()
             if (info == emptyInfo) {
                 throw IllegalStateException("Tried to store empty data!")
@@ -122,24 +128,21 @@ class SQLStorageProvider : AccountStorageProvider {
             if (info == emptyInfo) {
                 throw IllegalStateException("Tried to store empty data!")
             }
+            if (updatedFields.isEmpty()) return
 
-            compiledUpdateInfoQuery.setString(1, info.username)
-            compiledUpdateInfoQuery.setString(2, info.password)
-            compiledUpdateInfoQuery.setInt(3, info.rights)
-            compiledUpdateInfoQuery.setInt(4, info.credits)
-            compiledUpdateInfoQuery.setString(5, info.ip)
-            compiledUpdateInfoQuery.setLong(6, info.muteEndTime)
-            compiledUpdateInfoQuery.setLong(7, info.banEndTime)
-            compiledUpdateInfoQuery.setString(8, info.contacts)
-            compiledUpdateInfoQuery.setString(9, info.blocked)
-            compiledUpdateInfoQuery.setString(10, info.clanName)
-            compiledUpdateInfoQuery.setString(11, info.currentClan)
-            compiledUpdateInfoQuery.setString(12, info.clanReqs)
-            compiledUpdateInfoQuery.setLong(13, info.timePlayed)
-            compiledUpdateInfoQuery.setLong(14, info.lastLogin)
-            compiledUpdateInfoQuery.setBoolean(15, info.online)
-            compiledUpdateInfoQuery.setInt(16, info.uid)
+            var fieldIndex = 1
+            for (updatedFieldIndex in updatedFields) {
+                when(val data = rawData[updatedFieldIndex]) {
+                    is String -> compiledUpdateInfoQuery.setString(fieldIndex++, data)
+                    is Int -> compiledUpdateInfoQuery.setInt(fieldIndex++, data)
+                    is Boolean -> compiledUpdateInfoQuery.setBoolean(fieldIndex++, data)
+                    is Long -> compiledUpdateInfoQuery.setLong(fieldIndex++, data)
+                }
+            }
+            compiledUpdateInfoQuery.setInt(fieldIndex, info.uid)
             compiledUpdateInfoQuery.execute()
+
+            info.initialValues = rawData
         }
     }
 
@@ -193,22 +196,36 @@ class SQLStorageProvider : AccountStorageProvider {
                 "online" +
                 ") VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
 
-        private const val updateInfoQuery = "UPDATE members SET " +
-                "username = ?," +
-                "password = ?," +
-                "rights = ?," +
-                "credits = ?," +
-                "lastGameIp = ?," +
-                "muteTime = ?," +
-                "banTime = ?," +
-                "contacts = ?," +
-                "blocked = ?," +
-                "clanName = ?," +
-                "currentClan = ?," +
-                "clanReqs = ?," +
-                "timePlayed = ?," +
-                "lastLogin = ?," +
-                "online = ?" +
-                " WHERE uid = ?;"
+        private fun buildUpdateInfoQuery(updatedIndices: ArrayList<Int>) : String {
+            val sb = StringBuilder("UPDATE members SET ")
+            val validIndices = updatedIndices.filter { it in UPDATE_QUERY_FIELDS.keys }
+            for ((index, updatedIndex) in validIndices.withIndex()) {
+                sb.append(UPDATE_QUERY_FIELDS[updatedIndex] ?: continue)
+                sb.append(" = ?")
+                if (index < validIndices.size - 1)
+                    sb.append(",")
+            }
+            sb.append(" WHERE uid = ?;")
+            return sb.toString()
+        }
+
+        //Maps UserAccountInfo updated field indices to the column name for SQL. Mini ORM I guess?
+        private val UPDATE_QUERY_FIELDS = mapOf<Int, String>(
+            0 to "username",
+            1 to "password",
+            3 to "rights",
+            4 to "credits",
+            6 to "lastGameIp",
+            7 to "muteTime",
+            8 to "banTime",
+            9 to "contacts",
+            10 to "blocked",
+            11 to "clanName",
+            12 to "currentClan",
+            13 to "clanReqs",
+            14 to "timePlayed",
+            15 to "lastLogin",
+            16 to "online"
+        )
     }
 }
