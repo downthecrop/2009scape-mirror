54d7b669c6ab65e553bb65e1640a25a4a2323b00
enosich <evansaccounts@outlook.com>
2022-05-30 18:23:49 -0400

    Microshaft Winblows update 2: batch doesn't like non-conditionals

--- run-server.bat
+++ run-server.bat
@@ -1,13 +1,14 @@
+@echo off
 cd Server
 
 if NOT exist hasRan.txt (
     .\mvnw.cmd clean
     copy NUL hasRan.txt
     .\mvnw.cmd package -DskipTests
-    xcopy target\*-with-dependencies.jar server.jar
-    java -jar server.jar`
+    xcopy /Y target\*-with-dependencies.jar server.jar*
+    java -jar server.jar
 ) ELSE (
     .\mvnw.cmd package -DskipTests
-    xcopy target\*-with-dependencies.jar server.jar
+    xcopy /Y target\*-with-dependencies.jar server.jar*
     java -jar server.jar
 )
