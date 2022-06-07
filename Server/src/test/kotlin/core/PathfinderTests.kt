406bf0211a4059e501109d7eb500549c86c4aa09
ceikry <ironceikry@gmail.com>
2022-06-07 17:38:05 -0500

    start of tests

---
+++ Server/src/test/kotlin/core/PathfinderTests.kt
@@ -0,0 +1,12 @@
+package core
+
+import TestUtils
+import org.junit.jupiter.api.Test
+
+class PathfinderTests {
+    companion object {init {TestUtils.preTestSetup()}}
+
+    @Test fun getOccupiedTilesShouldReturnCorrectSetOfTilesThatAnObjectOccupiesAtAllRotations() {
+        
+    }
+}
