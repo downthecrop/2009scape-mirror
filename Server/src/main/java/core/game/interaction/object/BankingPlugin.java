b0438e5604d838e6a2d870abc5fbd1d19dbc496a
vddCore <t.cichon.64@outlook.com>
2022-06-13 19:35:08 +0200

    Restore gender detection unintentionally removed during a rebase.

--- Server/src/main/java/core/game/interaction/object/BankingPlugin.java
+++ Server/src/main/java/core/game/interaction/object/BankingPlugin.java
@@ -157,7 +157,7 @@ public final class BankingPlugin extends OptionHandler {
                     stage = 11;
                     break;
                 case 11:
-                    player(FacialExpression.LAUGH,"Well of course I can, a man of my","status could afford a measly bank fee");
+                    player(FacialExpression.LAUGH,"Well of course I can, a " + (player.isMale() ? "man" : "woman") + " of my","status could afford a measly bank fee");
                     stage = 12;
                     break;
                 case 12:
