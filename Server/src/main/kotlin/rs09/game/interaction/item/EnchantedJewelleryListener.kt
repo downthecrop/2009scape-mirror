f1ece41b370a07fddd340ea17931d5a4af0225ba
downthecrop <bryce@mathx.ca>
2022-06-15 19:41:18 -0700

    formatting

--- Server/src/main/kotlin/rs09/game/interaction/item/EnchantedJewelleryListener.kt
+++ Server/src/main/kotlin/rs09/game/interaction/item/EnchantedJewelleryListener.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 /**
  * Listener for enchanted jewellery options
- * @author Ceikry
+ * @author Ceikry & downthecrop
  */
 class EnchantedJewelleryListener : InteractionListener {
     val ids: IntArray
@@ -27,41 +27,42 @@ class EnchantedJewelleryListener : InteractionListener {
     }
 
     override fun defineListeners() {
-        on(ids,ITEM,"operate"){ player, node ->
-            handle(player,node,true)
+        on(ids, ITEM, "operate") { player, node ->
+            handle(player, node, true)
             return@on true
         }
-        on(ids,ITEM,"rub"){ player, node ->
-            handle(player,node,false)
-            return@on true;
+        on(ids, ITEM, "rub") { player, node ->
+            handle(player, node, false)
+            return@on true
         }
     }
-    private fun handle(player: Player, node: Node, isEquipped: Boolean){
+
+    private fun handle(player: Player, node: Node, isEquipped: Boolean) {
         player.pulseManager.current.stop()
         val item = node.asItem()
         val jewellery = EnchantedJewellery.forItem(item)
         if (jewellery != null) {
-            if (jewellery.isLastItemId(jewellery.getItemIndex(item)) && !jewellery.isCrumble) {
-                sendMessage(player,"The ${jewellery.getJewelleryType(item)} has lost its charge.")
-                sendMessage(player,"It will need to be recharged before you can use it again.")
+            if (jewellery.isLastItemIndex(jewellery.getItemIndex(item)) && !jewellery.isCrumble) {
+                sendMessage(player, "The ${jewellery.getJewelleryType(item)} has lost its charge.")
+                sendMessage(player, "It will need to be recharged before you can use it again.")
                 return
             }
-            sendMessage(player,"You rub the ${jewellery.getJewelleryType(item)}...")
-            if (jewellery.options.isNotEmpty()) {
-                player.dialogueInterpreter.open(object : DialogueFile(){
-                    override fun handle(componentID: Int, buttonID: Int) {
-                        when(stage){
-                            START_DIALOGUE -> {
-                                interpreter!!.sendOptions("Where would you like to go?", *jewellery.options)
-                                stage++
-                            }
-                            1 -> end().also{ jewellery.use(player, item, buttonID - 1,isEquipped) }
-                        }
-                    }
-                })
-            } else {
+            sendMessage(player, "You rub the ${jewellery.getJewelleryType(item)}...")
+            if (jewellery.options.isEmpty()) {
                 jewellery.use(player, item, 0, isEquipped)
+                return
             }
+            player.dialogueInterpreter.open(object : DialogueFile() {
+                override fun handle(componentID: Int, buttonID: Int) {
+                    when (stage) {
+                        START_DIALOGUE -> {
+                            interpreter!!.sendOptions("Where would you like to go?", *jewellery.options)
+                            stage++
+                        }
+                        1 -> end().also { jewellery.use(player, item, buttonID - 1, isEquipped) }
+                    }
+                }
+            })
         }
     }
 }
