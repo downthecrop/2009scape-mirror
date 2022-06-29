bead5f8f118aa6faf9a2cad73a1d6251df846271
vddCore <t.cichon.64@outlook.com>
2022-06-29 19:40:07 +0200

    Tweak the code according to Ceikry's tips.

--- Server/src/main/kotlin/rs09/game/node/entity/npc/city/piscatoris/ArnoldLydsporDialogue.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/npc/city/piscatoris/ArnoldLydsporDialogue.kt
@@ -9,6 +9,7 @@ import core.game.node.entity.player.Player
 import core.plugin.Initializable
 import org.rs09.consts.Items
 import org.rs09.consts.NPCs
+import rs09.game.content.dialogue.Topic
 import rs09.game.content.global.shops.Shops
 import rs09.game.node.entity.npc.other.banks.BankerNPC
 import rs09.tools.END_DIALOGUE
@@ -17,42 +18,26 @@ import rs09.tools.START_DIALOGUE
 /**
  * Provides the regular dialogue for Arnold Lydspor.
  * TODO: Swan Song quest will need a special case handling.
+ *
+ * @author vddCore
  */
 @Initializable
 class ArnoldLydsporDialogue(player: Player? = null) : DialoguePlugin(player) {
     override fun open(vararg args: Any?): Boolean {
         npc = args[0] as NPC
-        stage = START_DIALOGUE
         npc(FacialExpression.FRIENDLY, "Ah, you come back! What you want from Arnold, heh?")
         return true
     }
 
     override fun handle(interfaceId: Int, buttonId: Int): Boolean {
         when (stage) {
-            START_DIALOGUE -> options(
-                "Can you open my bank account, please?",
-                "I'd like to check my bank PIN settings.",
-                "I'd like to collect items.",
-                "Would you like to trade?",
-                "Nothing, I just came to chat."
-            ).also { stage++ }
-
-            1 -> when (buttonId) {
-                1 -> player("Can you open my bank account, please?")
-                    .also { stage = 2 }
-
-                2 -> player("I'd like to check my bank PIN settings.")
-                    .also { stage = 3 }
-
-                3 -> player("I'd like to collect items.")
-                    .also { stage = 4 }
-
-                4 -> player("Would you like to trade?")
-                    .also { stage = 5 }
-
-                5 -> player("Nothing, I just came to chat.")
-                    .also { stage = 7 }
-            }
+            START_DIALOGUE -> showTopics(
+                Topic("Can you open my bank account, please?", 2),
+                Topic(FacialExpression.NEUTRAL, "I'd like to check my bank PIN settings.", 3),
+                Topic(FacialExpression.NEUTRAL, "I'd like to collect items.", 4),
+                Topic("Would you like to trade?", 5),
+                Topic(FacialExpression.FRIENDLY, "Nothing, I just came to chat.", 7)
+            )
 
             2 -> BankerNPC.attemptBank(player, npc)
                 .also { end() }
@@ -72,11 +57,10 @@ class ArnoldLydsporDialogue(player: Player? = null) : DialoguePlugin(player) {
             6 -> Shops.shopsByNpc[NPCs.ARNOLD_LYDSPOR_3824]?.openFor(player)
                 .also { end() }
 
-            7 -> npc(
-                    FacialExpression.FRIENDLY,
-                    "Ah, that is nice - always I like to chat, but",
-                    "Herr Caranos tell me to get back to work!",
-                    "Here, you been nice, so have a present."
+            7 -> npcl(FacialExpression.FRIENDLY,
+                    "Ah, that is nice - always I like to chat, but"
+                    + " Herr Caranos tell me to get back to work!"
+                    + " Here, you been nice, so have a present."
                 ).also { stage++ }
 
             8 -> sendItemDialogue(player, Items.CABBAGE_1965, "Arnold gives you a cabbage.")
@@ -93,13 +77,11 @@ class ArnoldLydsporDialogue(player: Player? = null) : DialoguePlugin(player) {
 
             11 -> player(FacialExpression.NEUTRAL, "Um... Thanks!")
                  .also { stage = END_DIALOGUE }
-
-            END_DIALOGUE -> end()
         }
 
         return true
     }
 
     override fun newInstance(player: Player?): DialoguePlugin = ArnoldLydsporDialogue(player)
-    override fun getIds(): IntArray = ArnoldLydsporNPC.NPC_IDS
+    override fun getIds(): IntArray = ArnoldLydsporListener.NPC_IDS
 }
