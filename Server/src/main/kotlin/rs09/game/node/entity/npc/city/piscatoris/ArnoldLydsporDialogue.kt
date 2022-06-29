5b6979f53eb0f086a2bd3c37aeca7eb4c217f89e
vddCore <t.cichon.64@outlook.com>
2022-06-29 06:23:17 +0200

    Fix incorrect dialogue handling.

--- Server/src/main/kotlin/rs09/game/node/entity/npc/city/piscatoris/ArnoldLydsporDialogue.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/npc/city/piscatoris/ArnoldLydsporDialogue.kt
@@ -38,46 +38,61 @@ class ArnoldLydsporDialogue(player: Player? = null) : DialoguePlugin(player) {
             ).also { stage++ }
 
             1 -> when (buttonId) {
-                1 -> BankerNPC.attemptBank(player, npc)
-                    .also { end() }
-
-                2 -> /* TODO change when PIN management is re-worked
-                      * TODO and the relevant ContentAPI call is created
-                      */
-                    player.bankPinManager.openSettings()
-                    .also { end() }
-
-                3 -> BankerNPC.attemptCollect(player, npc)
-                    .also { end() }
-
-                4 -> npc(FacialExpression.FRIENDLY, "Ja, I have wide range of stock...")
+                1 -> player("Can you open my bank account, please?")
                     .also { stage = 2 }
 
-                5 -> npc(
+                2 -> player("I'd like to check my bank PIN settings.")
+                    .also { stage = 3 }
+
+                3 -> player("I'd like to collect items.")
+                    .also { stage = 4 }
+
+                4 -> player("Would you like to trade?")
+                    .also { stage = 5 }
+
+                5 -> player("Nothing, I just came to chat.")
+                    .also { stage = 7 }
+            }
+
+            2 -> BankerNPC.attemptBank(player, npc)
+                .also { end() }
+
+            /* TODO change when PIN management is re-worked
+             * TODO and the relevant ContentAPI call is created
+             */
+            3 -> player.bankPinManager.openSettings()
+                .also { end() }
+
+            4 -> BankerNPC.attemptCollect(player, npc)
+                .also { end() }
+
+            5 -> npc(FacialExpression.FRIENDLY, "Ja, I have wide range of stock...")
+                .also { stage++ }
+
+            6 -> Shops.shopsByNpc[NPCs.ARNOLD_LYDSPOR_3824]?.openFor(player)
+                .also { end() }
+
+            7 -> npc(
                     FacialExpression.FRIENDLY,
                     "Ah, that is nice - always I like to chat, but",
                     "Herr Caranos tell me to get back to work!",
                     "Here, you been nice, so have a present."
-                ).also { stage = 3 }
-            }
+                ).also { stage++ }
 
-            2 -> Shops.shopsByNpc[NPCs.ARNOLD_LYDSPOR_3824]?.openFor(player)
-                .also { end() }
-
-            3 -> sendItemDialogue(player, Items.CABBAGE_1965, "Arnold gives you a cabbage.")
+            8 -> sendItemDialogue(player, Items.CABBAGE_1965, "Arnold gives you a cabbage.")
                 .also {
                     addItemOrDrop(player, Items.CABBAGE_1965)
                     stage++
                 }
 
-            4 -> player(FacialExpression.HALF_THINKING, "A cabbage?")
+            9 -> player(FacialExpression.HALF_THINKING, "A cabbage?")
                 .also { stage++ }
 
-            5 -> npc("Ja, cabbage is good for you!")
-                .also { stage++ }
+            10 -> npc("Ja, cabbage is good for you!")
+                 .also { stage++ }
 
-            6 -> player(FacialExpression.NEUTRAL, "Um... Thanks!")
-                .also { stage = END_DIALOGUE }
+            11 -> player(FacialExpression.NEUTRAL, "Um... Thanks!")
+                 .also { stage = END_DIALOGUE }
 
             END_DIALOGUE -> end()
         }
