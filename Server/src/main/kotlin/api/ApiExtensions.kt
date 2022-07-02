81a12534bef5fac719a513351ec252cb9eaabec8
ceikry <ironceikry@gmail.com>
2022-07-02 15:07:43 -0500

    Clean step is DEAD

--- Server/src/main/kotlin/api/ApiExtensions.kt
+++ Server/src/main/kotlin/api/ApiExtensions.kt
@@ -22,3 +22,35 @@ fun Collection<IntArray>.toIntArray() : IntArray {
     this.forEach { arr -> arr.forEach { list.add(it) } }
     return list.toIntArray()
 }
+
+inline fun <reified T> Collection<T>.isLast(element: T) : Boolean {
+    return this.indexOf(element) == this.size - 1
+}
+
+inline fun <reified T> Collection<T>.getNext(element: T) : T {
+    val idx = this.indexOf(element)
+    return if (idx < this.size - 1)
+        this.elementAt(idx + 1)
+    else
+        element
+}
+
+inline fun <reified T> Collection<T>.isNextLast(element: T) : Boolean {
+    return this.isLast(this.getNext(element))
+}
+
+fun IntArray.isLast(element: Int) : Boolean {
+    return this.indexOf(element) == this.size - 1
+}
+
+fun IntArray.getNext(element: Int) : Int {
+    val idx = this.indexOf(element)
+    return if (idx < this.size - 1)
+        this.elementAt(idx + 1)
+    else
+        element
+}
+
+fun IntArray.isNextLast(element: Int) : Boolean {
+    return this.isLast(this.getNext(element))
+}
