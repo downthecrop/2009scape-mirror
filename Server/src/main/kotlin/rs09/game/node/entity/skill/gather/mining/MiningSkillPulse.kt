3083bc673aec46e43c38657c0150db6e4c6c957b
bushtail <evannosich@icloud.com>
2022-07-04 17:42:32 -0400

    Make MiningSkillPulse.kt use as many capi functions as possible - add capi function for getting the player's familiar skill boost.

--- Server/src/main/kotlin/rs09/game/node/entity/skill/gather/mining/MiningSkillPulse.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/gather/mining/MiningSkillPulse.kt
@@ -24,6 +24,7 @@ import core.game.world.update.flag.context.Animation
 import core.tools.RandomFunction
 import core.tools.StringUtils
 import org.rs09.consts.Items
+import org.rs09.consts.NPCs
 import rs09.game.node.entity.player.info.stats.STATS_BASE
 import rs09.game.node.entity.player.info.stats.STATS_ROCKS
 import rs09.game.node.entity.skill.skillcapeperks.SkillcapePerks
@@ -38,7 +39,6 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
     private var isMiningGems = false
     private var ticks = 0
     protected var resetAnimation = true
-    private val GEM_ROCKS = intArrayOf(23567, 23566, 23568, 23560, 23561, 23562)
 
     // Perfect Gold Ore in Witchhaven Dungeon (Family Crest)
     private val perfectGoldOreLocations = listOf(
@@ -50,7 +50,7 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
 
     fun message(type: Int) {
         if (type == 0) {
-            player.packetDispatch.sendMessage("You swing your pickaxe at the rock...")
+            sendMessage(player, "You swing your pickaxe at the rock...")
         }
     }
 
@@ -64,7 +64,7 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
 
     override fun stop() {
         if (resetAnimation) {
-            player.animate(Animation(-1, Animator.Priority.HIGH))
+            animate(player, Animation(-1, Animator.Priority.HIGH))
         }
         super.stop()
         message(1)
@@ -73,7 +73,7 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
     override fun start() {
         resource = MiningNode.forId(node.id)
         if (MiningNode.isEmpty(node.id)) {
-            player.packetDispatch.sendMessage("This rock contains no ore.")
+            sendMessage(player, "This rock contains no ore.")
         }
         if (resource == null) {
             return
@@ -97,27 +97,27 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
     }
 
     fun checkRequirements(): Boolean {
-        if (player.skills.getLevel(Skills.MINING) < resource!!.getLevel()) {
-            player.packetDispatch.sendMessage("You need a mining level of " + resource!!.getLevel() + " to mine this rock.")
+        if (getDynLevel(player, Skills.MINING) < resource!!.level) {
+            sendMessage(player, "You need a mining level of ${resource!!.level} to mine this rock.")
             return false
         }
         if (SkillingTool.getPickaxe(player) == null) {
-            player.packetDispatch.sendMessage("You do not have a pickaxe to use.")
+            sendMessage(player, "You do not have a pickaxe to use.")
             return false
         }
-        if (player.inventory.freeSlots() < 1) {
+        if (hasSpaceFor(player, Item(resource!!.reward))) {
             if(resource!!.identifier == 13.toByte()) {
-                player.dialogueInterpreter.sendDialogue("Your inventory is too full to hold any more gems.")
+                sendDialogue(player,"Your inventory is too full to hold any more gems.")
                 return false
             }
-            player.dialogueInterpreter.sendDialogue("Your inventory is too full to hold any more " + ItemDefinition.forId(resource!!.getReward()).name.lowercase() + ".")
+            sendDialogue(player,"Your inventory is too full to hold any more ${ItemDefinition.forId(resource!!.reward).name.lowercase()}.")
             return false
         }
         return true
     }
 
     fun animate() {
-        player.animate(SkillingTool.getPickaxe(player).animation)
+        animate(player, SkillingTool.getPickaxe(player).animation)
     }
 
     fun reward(): Boolean {
@@ -125,7 +125,7 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
             return false
         }
         if (node.id == 10041) {
-            player.dialogueInterpreter.sendDialogues(2574, FacialExpression.FURIOUS, if (RandomFunction.random(2) == 1) "You'll blow my cover! I'm meant to be hidden!" else "Will you stop that?")
+            sendNPCDialogue(player, NPCs.BANK_GUARD_2574,if (RandomFunction.random(2) == 1) "You'll blow my cover! I'm meant to be hidden!" else "Will you stop that?", FacialExpression.FURIOUS)
             return true
         }
         if (!checkReward()) {
@@ -144,19 +144,19 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
 
             //add experience
             val experience = resource!!.getExperience() * rewardAmount
-            player.skills.addExperience(Skills.MINING, experience, true)
+            rewardXP(player, Skills.MINING, experience)
 
             //Handle bracelet of clay
             if(reward == Items.CLAY_434){
-                val bracelet = player.equipment.get(EquipmentContainer.SLOT_HANDS)
+                val bracelet = getItemFromEquipment(player, EquipmentSlot.HANDS)
                 if(bracelet != null && bracelet.id == Items.BRACELET_OF_CLAY_11074){
                     if(bracelet.charge > 28) bracelet.charge = 28
                     bracelet.charge--
                     reward = Items.SOFT_CLAY_1761
-                    player.sendMessage("Your bracelet of clay softens the clay for you.")
+                    sendMessage(player, "Your bracelet of clay softens the clay for you.")
                     if(bracelet.charge <= 0){
-                        player.sendMessage("Your bracelet of clay crumbles to dust.")
-                        player.equipment.remove(bracelet)
+                        sendMessage(player, "Your bracelet of clay crumbles to dust.")
+                        removeItem(player, bracelet)
                     }
                 }
             }
@@ -164,33 +164,33 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
             //send the message for the resource reward
             if (isMiningGems) {
                 val gemName = ItemDefinition.forId(reward).name.lowercase()
-                player.sendMessage("You get " + (if (StringUtils.isPlusN(gemName)) "an" else "a") + " " + gemName + ".")
+                sendMessage(player, "You get ${(if (StringUtils.isPlusN(gemName)) "an" else "a")} ${gemName}.")
             } else {
-                player.packetDispatch.sendMessage("You get some " + ItemDefinition.forId(reward).name.lowercase() + ".")
+                sendMessage(player, "You get some ${resource!!.name.lowercase()}.")
             }
             //give the reward
-            player.inventory.add(Item(reward, rewardAmount))
-            var rocksMined = player.getAttribute("$STATS_BASE:$STATS_ROCKS",0)
-            player.setAttribute("/save:$STATS_BASE:$STATS_ROCKS",++rocksMined)
+            addItem(player, reward, rewardAmount)
+            var rocksMined = getAttribute(player, "$STATS_BASE:$STATS_ROCKS",0)
+            setAttribute(player, "/save:$STATS_BASE:$STATS_ROCKS",++rocksMined)
 
             //calculate bonus gem for mining
             if (!isMiningEssence) {
                 var chance = 282
                 var altered = false
-                if (Item(player.equipment.getId(12)).name.lowercase().contains("ring of wealth") || inEquipment(player, Items.RING_OF_THE_STAR_SPRITE_14652)) {
+                if (getItemFromEquipment(player, EquipmentSlot.RING)!!.name.lowercase().contains("ring of wealth") || inEquipment(player, Items.RING_OF_THE_STAR_SPRITE_14652)) {
                     chance = (chance / 1.5).toInt()
                     altered = true
                 }
-                val necklace = player.equipment[EquipmentContainer.SLOT_AMULET]
+                val necklace = getItemFromEquipment(player, EquipmentSlot.AMULET)
                 if (necklace != null && necklace.id in 1705..1713) {
                     chance = (chance / 1.5).toInt()
                     altered = true
                 }
                 if (RandomFunction.roll(chance)) {
                     val gem = GEM_REWARDS.random()
-                    player.packetDispatch.sendMessage("You find a " + gem.name + "!")
-                    if (!player.inventory.add(gem, player)) {
-                        player.packetDispatch.sendMessage("You do not have enough space in your inventory, so you drop the gem on the floor.")
+                    sendMessage(player,"You find a ${gem.name}!")
+                    if (!addItem(player, gem.id)) {
+                        sendMessage(player,"You do not have enough space in your inventory, so you drop the gem on the floor.")
                     }
                 }
             }
@@ -213,15 +213,15 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
             when (reward) {
                 Items.CLAY_434, Items.COPPER_ORE_436, Items.TIN_ORE_438, Items.LIMESTONE_3211, Items.BLURITE_ORE_668, Items.IRON_ORE_440, Items.ELEMENTAL_ORE_2892, Items.SILVER_ORE_442, Items.COAL_453 -> if (player.achievementDiaryManager.armour >= 0 && RandomFunction.random(100) <= 10) {
                     amount += 1
-                    player.sendMessage("The Varrock armour allows you to mine an additional ore.")
+                    sendMessage(player,"The Varrock armour allows you to mine an additional ore.")
                 }
                 Items.GOLD_ORE_444, Items.GRANITE_500G_6979, Items.GRANITE_2KG_6981, Items.GRANITE_5KG_6983, Items.MITHRIL_ORE_447 -> if (player.achievementDiaryManager.armour >= 1 && RandomFunction.random(100) <= 10) {
                     amount += 1
-                    player.sendMessage("The Varrock armour allows you to mine an additional ore.")
+                    sendMessage(player, "The Varrock armour allows you to mine an additional ore.")
                 }
                 Items.ADAMANTITE_ORE_449 -> if (player.achievementDiaryManager.armour >= 2 && RandomFunction.random(100) <= 10) {
                     amount += 1
-                    player.sendMessage("The Varrock armour allows you to mine an additional ore.")
+                    sendMessage(player, "The Varrock armour allows you to mine an additional ore.")
                 }
             }
         }
@@ -229,7 +229,7 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
         //check for bonus ore from shooting star buff
         if (player.hasActiveState("shooting-star")) {
             if (RandomFunction.getRandom(5) == 3) {
-                player.packetDispatch.sendMessage("...you manage to mine a second ore thanks to the Star Sprite.")
+                sendMessage(player, "...you manage to mine a second ore thanks to the Star Sprite.")
                 amount += 1
             }
         }
@@ -242,8 +242,8 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
         if (resource == MiningNode.SANDSTONE || resource == MiningNode.GRANITE) {
             val value = RandomFunction.randomize(if (resource == MiningNode.GRANITE) 3 else 4)
             reward += value shl 1
-            player.skills.addExperience(Skills.MINING, value * 10.toDouble(), true)
-        } else if (isMiningEssence && player.skills.getLevel(Skills.MINING) >= 30) {
+            rewardXP(player, Skills.MINING, value * 10.toDouble())
+        } else if (isMiningEssence && getDynLevel(player, Skills.MINING) >= 30) {
             reward = 7936
         } else if (isMiningGems) {
             reward = RandomFunction.rollWeightedChanceTable(MiningNode.gemRockGems).id
@@ -256,14 +256,13 @@ class MiningSkillPulse(private val player: Player, private val node: Node) : Pul
      * @return `True` if so.
      */
     private fun checkReward(): Boolean {
-        val skill = Skills.MINING
-        val level = 1 + player.skills.getLevel(skill) + player.familiarManager.getBoost(skill)
-        val hostRatio = Math.random() * (100.0 * resource!!.getRate())
+        val level = 1 + getDynLevel(player, Skills.MINING) + getFamiliarBoost(player, Skills.MINING)
+        val hostRatio = Math.random() * (100.0 * resource!!.rate)
         var toolRatio = SkillingTool.getPickaxe(player).ratio
         if(SkillcapePerks.isActive(SkillcapePerks.PRECISION_MINER,player)){
             toolRatio += 0.075
         }
-        val clientRatio = Math.random() * ((level - resource!!.getLevel()) * (1.0 + toolRatio))
+        val clientRatio = Math.random() * ((level - resource!!.level) * (1.0 + toolRatio))
         return hostRatio < clientRatio
     }
 
