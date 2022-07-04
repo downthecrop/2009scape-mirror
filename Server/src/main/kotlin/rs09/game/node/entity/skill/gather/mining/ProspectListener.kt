74f545715e994a0520b4047aaec85b353d0d1180
bushtail <evannosich@icloud.com>
2022-07-04 16:33:42 -0400

    use MiningNode, not SkillingResource.

--- Server/src/main/kotlin/rs09/game/node/entity/skill/gather/mining/ProspectListener.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/gather/mining/ProspectListener.kt
@@ -1,8 +1,8 @@
 package rs09.game.node.entity.skill.gather.mining
 
 import api.*
-import core.game.node.entity.skill.gather.SkillingResource
 import core.game.node.entity.skill.gather.mining.MiningNode
+import core.game.node.item.Item
 import core.game.system.task.Pulse
 import rs09.game.interaction.InteractionListener
 
@@ -14,7 +14,13 @@ class ProspectListener : InteractionListener {
 
     override fun defineListeners() {
         on(SCENERY, "prospect") { player, node ->
-            val rock = SkillingResource.forId(node.asScenery().id)
+            val rock = MiningNode.forId(node.asScenery().id)
+
+            if(rock == null) {
+                sendMessage(player, "There is no ore currently available in this rock.")
+                return@on false
+            }
+
             /** Check if the rock contains gems */
             if(MiningNode.forId(node.id).identifier == 13.toByte()) {
                 sendMessage(player,"You examine the rock for ores...")
@@ -28,18 +34,13 @@ class ProspectListener : InteractionListener {
                 return@on true
             }
 
-            if(rock == null) {
-                sendMessage(player, "There is no ore currently available in this rock.")
-                return@on false
-            }
-
             /** If it doesn't contain gems */
             else {
                 sendMessage(player,"You examine the rock for ores...")
                 /** Get the name of the rock's reward and sends a message to the player */
                 player.pulseManager.run(object : Pulse(3) {
                     override fun pulse(): Boolean {
-                        sendMessage(player, "This rock contains ${itemDefinition(rock.reward).name.lowercase()}.")
+                        sendMessage(player, "This rock contains ${Item(rock.reward).name.lowercase()}.")
                         return true
                     }
                 })
