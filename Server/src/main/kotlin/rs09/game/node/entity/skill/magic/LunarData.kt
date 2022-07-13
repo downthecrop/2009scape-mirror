ac432998bd7c5fec726dad6c105da69d140ba12f
bushtail <evannosich@icloud.com>
2022-07-13 15:34:00 -0400

    Fix improper rebase.

---
+++ Server/src/main/kotlin/rs09/game/node/entity/skill/magic/LunarData.kt
@@ -0,0 +1,44 @@
+package rs09.game.node.entity.skill.magic
+
+import core.game.world.update.flag.context.Animation
+import core.game.world.update.flag.context.Graphics
+import org.rs09.consts.Items
+
+val BAKE_PIE_ANIM = Animation(4413)
+val BAKE_PIE_GFX = Graphics(746,75)
+val STATSPY_ANIM = Animation(6293)
+val STATSPY_GFX = Graphics(1059)
+val CURE_PLANT_ANIM = Animation(4409)
+val CURE_PLANT_GFX = Graphics(742,100)
+val NPC_CONTACT_ANIM = Animation(4413)
+val NPC_CONTACT_GFX = Graphics(730,130)
+val PLANK_MAKE_ANIM = Animation(6298)
+val PLANK_MAKE_GFX = Graphics(1063, 120)
+val STRING_JEWELLERY_ANIM = Animation(4412)
+val STRING_JEWELLERY_GFX = Graphics(728, 100)
+val SUPERGLASS_MAKE_ANIM = Animation(4413)
+val SUPERGLASS_MAKE_GFX = Graphics(729, 120)
+
+
+enum class JewelleryString(val unstrung: Int, val strung: Int) {
+    GOLD(Items.GOLD_AMULET_1673, Items.GOLD_AMULET_1692),
+    SAPPHIRE(Items.SAPPHIRE_AMULET_1675, Items.SAPPHIRE_AMULET_1694),
+    EMERALD(Items.EMERALD_AMULET_1677, Items.EMERALD_AMULET_1696),
+    RUBY(Items.RUBY_AMULET_1679, Items.RUBY_AMULET_1698),
+    DIAMOND(Items.DIAMOND_AMULET_1681, Items.DIAMOND_AMULET_1700),
+    DRAGONSTONE(Items.DRAGONSTONE_AMMY_1683, Items.DRAGONSTONE_AMMY_1702),
+    ONYX(Items.ONYX_AMULET_6579, Items.ONYX_AMULET_6581),
+    SALVE(Items.SALVE_SHARD_4082, Items.SALVE_AMULET_4081),
+    HOLY(Items.UNSTRUNG_SYMBOL_1714, Items.UNBLESSED_SYMBOL_1716),
+    UNHOLY(Items.UNSTRUNG_EMBLEM_1720, Items.UNPOWERED_SYMBOL_1722);
+    companion object {
+        val productOfString = values().associate { it.unstrung to it.strung }
+        fun forId(id : Int) : Int {
+            return productOfString[id]!!
+        }
+
+        fun unstrungContains(id: Int): Boolean {
+            return productOfString.contains(id)
+        }
+    }
+}
