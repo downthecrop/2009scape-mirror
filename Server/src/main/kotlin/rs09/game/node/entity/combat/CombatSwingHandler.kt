93f00462b999221474cd3d7803ae19d74fc208f7
Ceikry <ironceikry@gmail.com>
2022-09-11 12:38:44 +0000

    Fixed a potential exploit where pvp-specific behaviors were being taken into account for PvM, resulting in potential non-halberd melee safespots

--- Server/src/main/kotlin/rs09/game/node/entity/combat/CombatSwingHandler.kt
+++ Server/src/main/kotlin/rs09/game/node/entity/combat/CombatSwingHandler.kt
@@ -234,7 +234,7 @@ abstract class CombatSwingHandler(var type: CombatStyle?) {
             entity.debug("Adjusting attack style")
         }
         if (entity.location == victim.location) {
-            return if (entity.index < victim.index && victim.properties.combatPulse.getVictim() === entity) {
+            return if (entity is Player && victim is Player && entity.clientIndex < victim.clientIndex && victim.properties.combatPulse.getVictim() === entity) {
                 InteractionType.STILL_INTERACT
             } else InteractionType.NO_INTERACT
         }
