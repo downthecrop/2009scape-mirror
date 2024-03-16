6cb4f118ea5432d43d4b7e9bd53a9630d10fb11b
Jordan Christiansen <xordspar0@gmail.com>
2024-03-16 12:17:22 +0000

    Bats now only drop bat wings during Rag and Bone Man quest

--- Server/src/main/content/region/misthalin/silvarea/quest/ragandboneman/BatBehavior.kt
+++ Server/src/main/content/region/misthalin/silvarea/quest/ragandboneman/BatBehavior.kt
@@ -10,28 +10,19 @@ import core.tools.RandomFunction
 import org.rs09.consts.Items
 import org.rs09.consts.NPCs
 
-class GiantBatBehavior : NPCBehavior(*giantBatIds) {
+class BatBehavior : NPCBehavior(*batIds) {
     companion object {
-        private val giantBatIds = intArrayOf(
-                NPCs.GIANT_BAT_78,
-                NPCs.GIANT_BAT_1005,
-                NPCs.GIANT_BAT_2482,
-                NPCs.GIANT_BAT_2483,
-                NPCs.GIANT_BAT_2484,
-                NPCs.GIANT_BAT_2485,
-                NPCs.GIANT_BAT_2486,
-                NPCs.GIANT_BAT_2487,
-                NPCs.GIANT_BAT_2488,
-                NPCs.GIANT_BAT_3711,
+        private val batIds = intArrayOf(
+                NPCs.BAT_412
         )
     }
 
     override fun onDropTableRolled(self: NPC, killer: Entity, drops: ArrayList<Item>) {
         super.onDropTableRolled(self, killer, drops)
-        // Drops the Giant Bat Wing during Rag and Bone Man quest
+        // Drops the Bat Wing during Rag and Bone Man quest
         if (killer is Player && isQuestInProgress(killer, RagAndBoneMan.questName, 1, 99)) {
             if(RandomFunction.roll(4)) {
-                drops.add(Item(Items.GIANT_BAT_WING_7827));
+                drops.add(Item(Items.BAT_WING_7833));
             }
         }
     }
