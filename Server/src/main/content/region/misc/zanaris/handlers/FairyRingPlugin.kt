370d9ef423ea72e11dc4f4cac31e7358a7697ad4
Kamja Imo <vjsigler@gmail.com>
2024-06-27 14:11:43 +0000

    Removed quest completion check on fairy ring exit from Zanaris

--- Server/src/main/content/region/misc/zanaris/handlers/FairyRingPlugin.kt
+++ Server/src/main/content/region/misc/zanaris/handlers/FairyRingPlugin.kt
@@ -14,9 +14,10 @@ import core.game.interaction.IntType
  * @author Ceikry
  */
 
-private val RINGS = intArrayOf(12003, 12095, 14058, 14061, 14064, 14067, 14070, 14073, 14076, 14079, 14082, 14085, 14088, 14091, 14094, 14097, 14100, 14103, 14106, 14109, 14112, 14115, 14118, 14121, 14124, 14127, 14130, 14133, 14136, 14139, 14142, 14145, 14148, 14151, 14154, 14157, 14160, 16181, 16184, 23047, 27325, 37727)
+private val RINGS = intArrayOf(12095, 14058, 14061, 14064, 14067, 14070, 14073, 14076, 14079, 14082, 14085, 14088, 14091, 14094, 14097, 14100, 14103, 14106, 14109, 14112, 14115, 14118, 14121, 14124, 14127, 14130, 14133, 14136, 14139, 14142, 14145, 14148, 14151, 14154, 14157, 14160, 16181, 16184, 23047, 27325, 37727)
 private const val MAIN_RING = 12128
 private const val ENTRY_RING = 12094
+private const val MARKETPLACE_RING = 12003
 
 class FairyRingPlugin : InteractionListener {
 
@@ -24,7 +25,8 @@ class FairyRingPlugin : InteractionListener {
 
         on(RINGS, IntType.SCENERY, "use"){ player, _ ->
             if (!fairyMagic(player)) return@on true
-            player.teleporter.send(Location.create(2412, 4434, 0), TeleportType.FAIRY_RING)
+            val mainRingLocation = Location.create(2412, 4434, 0)
+            player.teleporter.send(mainRingLocation, TeleportType.FAIRY_RING)
             return@on true
         }
         on(MAIN_RING, IntType.SCENERY, "use"){ player, _ ->
@@ -33,8 +35,13 @@ class FairyRingPlugin : InteractionListener {
             return@on true
         }
         on(ENTRY_RING, IntType.SCENERY, "use") { player, _ ->
-            if (!fairyMagic(player)) return@on true
-            player.teleporter.send(Location.create(3203, 3168, 0), TeleportType.FAIRY_RING)
+            val lumbridgeSwampShed = Location.create(3203, 3168, 0)
+            player.teleporter.send(lumbridgeSwampShed, TeleportType.FAIRY_RING)
+            return@on true
+        }
+        on(MARKETPLACE_RING, IntType.SCENERY, "use"){ player, _ ->
+            val alKharidBank = Location.create(3260, 3156, 0)
+            player.teleporter.send(alKharidBank, TeleportType.FAIRY_RING)
             return@on true
         }
 
