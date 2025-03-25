1b0a7d5fcacb9ab9c70fa0b3de7a4ed2fba08a27
Alex Page <alex@anpage.me>
2025-03-25 09:51:47 +0000

    Fixed random events returning players to Lumbridge

--- Server/src/main/content/global/ame/KidnapHelper.kt
+++ Server/src/main/content/global/ame/KidnapHelper.kt
@@ -15,6 +15,11 @@ fun kidnapPlayer(player: Player, loc: Location, type: TeleportType) {
 }
 
 fun returnPlayer(player: Player) {
+    // Prevent returning more than once and sending the player back to HOME_LOCATION
+    if (getAttribute<Location?>(player, "kidnapped-by-random", null) == null) {
+        return
+    }
+
     player.locks.unlockTeleport()
     val destination = getAttribute(player, "/save:original-loc", ServerConstants.HOME_LOCATION ?: Location.create(3222, 3218, 0))
     teleport(player, destination)
