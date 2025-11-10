f1f5a97859117cbc693b42d70cc8f551bbcb5755
Kennynes <kennynes1@gmail.com>
2025-11-10 12:28:13 +0000

    Made the Black Demon cutscene more authentic and fixed Glough's chathead in the cutscene

--- Server/src/main/content/region/kandarin/quest/grandtree/BlackDemonCutscene.kt
+++ Server/src/main/content/region/kandarin/quest/grandtree/BlackDemonCutscene.kt
@@ -1,20 +1,16 @@
 package content.region.kandarin.quest.grandtree
 
-import content.region.misthalin.dorgeshuun.quest.thelosttribe.LostTribeCutscene
 import core.ServerConstants
-import core.api.openDialogue
 import core.api.sendChat
-import core.api.sendDialogue
-import core.api.sendMessage
 import core.game.activity.Cutscene
 import core.game.dialogue.FacialExpression
 import core.game.node.entity.player.Player
 import core.game.world.map.Direction
 import core.game.world.map.Location
-import core.game.global.action.DoorActionHandler
-import core.game.node.entity.npc.NPC
+import core.game.world.update.flag.context.Animation
 import org.rs09.consts.NPCs
 
+// Source video https://www.youtube.com/watch?v=LS1Xtuz0sLA
 class BlackDemonCutscene(player: Player) : Cutscene(player) {
     override fun setup() {
         setExit(Location.create(2491, 9864, 0))
@@ -23,7 +19,6 @@ class BlackDemonCutscene(player: Player) : Cutscene(player) {
         }
         loadRegion(9882)
         addNPC(NPCs.GLOUGH_671, 48, 8, Direction.WEST)
-        addNPC(NPCs.BLACK_DEMON_677, 43, 9, Direction.EAST)
     }
 
     override fun runStage(stage: Int) {
@@ -40,6 +35,8 @@ class BlackDemonCutscene(player: Player) : Cutscene(player) {
                 rotateCamera(0, 0)
                 sendChat(player, "Hello?")
                 player.face(getNPC(NPCs.GLOUGH_671)!!)
+                addNPC(NPCs.BLACK_DEMON_677, 39, 8, Direction.EAST)
+                move(getNPC(NPCs.BLACK_DEMON_677)!!, 41, 8)
                 timedUpdate(3)
             }
             2 -> {
@@ -53,7 +50,7 @@ class BlackDemonCutscene(player: Player) : Cutscene(player) {
                 timedUpdate(10)
             }
             4 -> {
-                moveCamera(55, 4,2000)
+                moveCamera(55, 4, 2000)
                 rotateCamera(55, 6)
                 timedUpdate(4)
             }
@@ -62,24 +59,27 @@ class BlackDemonCutscene(player: Player) : Cutscene(player) {
                 playerDialogueUpdate(FacialExpression.SCARED, "Glough?")
             }
             6 -> {
-                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.ANGRY, "You really are becoming a headache! Well, at least now you can die knowing you were right, it will save me having to hunt you down like all the other human filth of " + ServerConstants.SERVER_NAME + "!")
+                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.OLD_ANGRY1, "You really are becoming a headache! Well, at least now you can die knowing you were right, it will save me having to hunt you down like all the other human filth of " + ServerConstants.SERVER_NAME + "!")
             }
             7 -> {
                 playerDialogueUpdate(FacialExpression.SCARED, "You're crazy, Glough!")
             }
             8 -> {
-                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.ANGRY, "Bah! Well, soon you'll see, the gnomes are ready to fight. In three weeks this tree will be dead wood, in ten weeks it will be 30 battleships! Finally we will rid the world of the disease called humanity!")
+                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.OLD_ANGRY1, "Bah! Well, soon you'll see, the gnomes are ready to fight. In three weeks this tree will be dead wood, in ten weeks it will be 30 battleships! Finally we will rid the world of the disease called humanity!")
             }
             9 -> {
                 playerDialogueUpdate(FacialExpression.SCARED, "What makes you think I'll let you get away with it?")
             }
             10 -> {
-                moveCamera(47,9)
-                rotateCamera(40, 9)
-                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.ANGRY, "Fool...meet my little friend!")
+                moveCamera(55, 8, 700)
+                rotateCamera(48, 8)
+                val demon = getNPC(NPCs.BLACK_DEMON_677)!!
+                move(demon, 46, 8)
+                demon.animate(Animation(64, 140))
+                dialogueUpdate(NPCs.GLOUGH_671, FacialExpression.OLD_ANGRY1, "Fool...meet my little friend!")
             }
             11 -> {
-                end{
+                end(fade = false){
                     BlackDemonNPC(NPCs.BLACK_DEMON_677, Location.create(2485, 9864, 0)).init()
                 }
             }
